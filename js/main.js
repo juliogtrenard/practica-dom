44889f563df80d88a9ebfe8346a617180c4f22d3
Julio González <juliogtrenard@gmail.com>
2025-10-27 13:33:08 +0100

    ADD codigo javascript y estilos

--- js/main.js
+++ js/main.js
@@ -1,106 +1,160 @@
 /* VARIABLES */
 /* capturar elementos DOM */
-const imagenBanner = document.querySelector('#bienvenidos img')
+const imagenBanner = document.querySelector("#bienvenidos img");
 /* Acceder al elemento del DOM para crear los artículos de viajes */
-
-
+const flexContainer = document.querySelector(".flexContainer");
+const fragment = document.createDocumentFragment();
 /* Acceder al elemento del DOM para crear añadir los options */
-
-const arrayBanners = [
-    {
-        id: 1,
-        src: 'assets/banner/1.jpg',
-        alt: 'Banner uno'
-    },
-    {
-        id: 2,
-        src: 'assets/banner/2.jpg',
-        alt: 'Banner uno'
-    },
-    {
-        id: 3,
-        src: 'assets/banner/3.jpg',
-        alt: 'Banner uno'
-    },
-    {
-        id: 4,
-        src: 'assets/banner/4.jpg',
-        alt: 'Banner uno'
-    },
-    {
-        id: 5,
-        src: 'assets/banner/5.jpg',
-        alt: 'Banner uno'
-    },
-    {
-        id: 6,
-        src: 'assets/banner/6.jpg',
-        alt: 'Banner uno'
-    },
-    {
-        id: 7,
-        src: 'assets/banner/7.jpg',
-        alt: 'Banner uno'
-    },
-    {
-        id: 8,
-        src: 'assets/banner/8.jpg',
-        alt: 'Banner uno'
-    },
-]
-const arrayViajes = {
-
-}
-const arrayDestinos = {}
-
+const destinos = document.querySelector("#select-destinos");
 
 /* EVENTOS */
 
 /* FUNCIONES */
-const aleatorio = () => {
-    const indice = '********'
-    return indice
-}
+const aleatorio = (array) => {
+  return Math.floor(Math.random() * array.length);
+};
 
 const pintarBanner = () => {
+  const arrayBanners = [
+    {
+      id: 1,
+      src: "assets/banner/1.jpg",
+      alt: "Atardecer",
+    },
+    {
+      id: 2,
+      src: "assets/banner/2.jpg",
+      alt: "Cielo",
+    },
+    {
+      id: 3,
+      src: "assets/banner/3.jpg",
+      alt: "Atardecer con nubes",
+    },
+    {
+      id: 4,
+      src: "assets/banner/4.jpg",
+      alt: "Campo y cielo",
+    },
+    {
+      id: 5,
+      src: "assets/banner/5.jpg",
+      alt: "Noche",
+    },
+    {
+      id: 6,
+      src: "assets/banner/6.jpg",
+      alt: "Montaña y nieve",
+    },
+    {
+      id: 7,
+      src: "assets/banner/7.jpg",
+      alt: "Atardecer y pájaros",
+    },
+    {
+      id: 8,
+      src: "assets/banner/8.jpg",
+      alt: "Flores",
+    },
+  ];
 
-    const indice = aleatorio()
-    const elemento = arrayBanners[0]
+  const indice = aleatorio(arrayBanners);
+  const elemento = arrayBanners[indice];
 
-    // console.log(elemento.alt)
-    // console.log(elemento.src)
+  imagenBanner.src = elemento.src;
+  imagenBanner.alt = elemento.alt;
+};
 
-    imagenBanner.setAttribute('src', elemento.src)
-    imagenBanner.alt = elemento.alt
-    // console.log(imagenBanner)
+const pintarCards = () => {
+  const arrayViajes = [
+    {
+      id: 1,
+      src: "assets/viajes/viajes-1.jpg",
+      alt: "Playa",
+      titulo: "Viaje 1",
+      texto: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    },
+    {
+      id: 2,
+      src: "assets/viajes/viajes-2.jpg",
+      alt: "Cabaña",
+      titulo: "Viaje 2",
+      texto: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    },
+    {
+      id: 3,
+      src: "assets/viajes/viajes-3.jpg",
+      alt: "Ubicaciones",
+      titulo: "Viaje 3",
+      texto: "Lorem ipsum dolor sit amet, consectetur",
+    },
+    {
+      id: 4,
+      src: "assets/viajes/viajes-4.jpg",
+      alt: "Casas",
+      titulo: "Viaje 4",
+      texto:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    {
+      id: 5,
+      src: "assets/viajes/viajes-5.jpg",
+      alt: "Puente",
+      titulo: "Viaje 5",
+      texto: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    },
+    {
+      id: 6,
+      src: "assets/viajes/viajes-6.jpg",
+      alt: "Montaña",
+      titulo: "Viaje 6",
+      texto:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    {
+      id: 7,
+      src: "assets/viajes/viajes-7.jpg",
+      alt: "Castillo",
+      titulo: "Viaje 7",
+      texto: "Lorem ipsum dolor sit amet, consectetur",
+    },
+  ];
 
-}
+  arrayViajes.forEach((elemento) => {
+    const articulo = document.createElement("ARTICLE");
+    articulo.classList.add("decoracionArticle");
+    const cajaImagen = document.createElement("DIV");
+    const imagenArticulo = document.createElement("IMG");
+    imagenArticulo.src = elemento.src;
+    imagenArticulo.alt = elemento.alt;
+    cajaImagen.append(imagenArticulo);
+    articulo.append(cajaImagen);
+    const titulo = document.createElement("H3");
+    titulo.innerText = elemento.titulo;
+    articulo.append(titulo);
+    const parrafo = document.createElement("P");
+    parrafo.innerText = elemento.texto;
+    articulo.append(parrafo);
+    fragment.append(articulo);
+  });
 
-const pintarCards = {
-    /* recorrer los elementos del array/*
-    /* por cada elemento crear:/*
-    /* un article   */
-    /* un div   */
-    /* una imgen asignando sus atributos   */
-    /* un h3 asignar su valor   */
-    /* un p asignar su valor   */
+  flexContainer.append(fragment);
+};
 
-    /* meter la imagen en el div */
-    /* meter el div en el artigle */
-    /* meter el h3 y el p en el article */
+const pintarDestinos = () => {
+  const arrayDestinos = ["Burgos", "Vitoria", "Barcelona", "Madrid", "Bilbao"];
 
-    /* añadir el artícle fragmento */
-    /* dejo de recorrer el array */
+  arrayDestinos.forEach((elemento) => {
+    const opcion = document.createElement("OPTION");
+    opcion.value = elemento;
+    opcion.innerText = elemento;
+    fragment.append(opcion);
+  });
 
-    /* añadir el fragmento al div flexContainer */
-
-}
-const pintarDestinos = {
-
-}
-
-
+  destinos.append(fragment);
+};
 
 /* INVOCACIÓN A LAS FUNCIONES */
-pintarBanner()
-// pintarCards()
+pintarBanner();
+pintarCards();
+pintarDestinos();
